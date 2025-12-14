707f067b0ca7f86ca8b0a184b9c1caf18151f6a5
Ben Rawlings <163949719+BenGameDev@users.noreply.github.com>
2025-12-14 18:50:40 +0000

    On hover cards raise
    When hovering on and off the cards, it lifts them up to be viewable and back down to be out of the way of the chess board

--- unity-3D-template/Assets/Scripts/Cards/DragUIElements.cs
+++ unity-3D-template/Assets/Scripts/Cards/DragUIElements.cs
@@ -1,14 +1,18 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class DragUIElements : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler
+public class DragUIElements : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler, IPointerEnterHandler, IPointerExitHandler
 {
     public RectTransform rectTransform;
+    public Vector2 hoverTransform;
+    public int siblingIndex;
 
     private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
+        siblingIndex = transform.GetSiblingIndex();
     }
 
     public void OnBeginDrag(PointerEventData eventData)
@@ -25,5 +29,17 @@ public class DragUIElements : MonoBehaviour, IBeginDragHandler, IDragHandler, IE
         LayoutRebuilder.ForceRebuildLayoutImmediate(transform.parent.GetComponent<RectTransform>());
     }
 
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition = new Vector2(rectTransform.anchoredPosition.x, hoverTransform.y);
+        this.GetComponent<LayoutElement>().ignoreLayout = true;
+        rectTransform.SetSiblingIndex(5);
+    }
 
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        LayoutRebuilder.ForceRebuildLayoutImmediate(transform.parent.GetComponent<RectTransform>());
+        rectTransform.SetSiblingIndex(siblingIndex);
+        this.GetComponent <LayoutElement>().ignoreLayout = false;
+    }
 }
